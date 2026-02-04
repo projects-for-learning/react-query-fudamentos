331e9b12dc5c2625763c2366c6782c16bc96cdaf
HenriquePereiraa <henrique802011@gmail.com>
2026-02-04 20:55:29 -0300

    feat: trabalhando com isLoading, isPeding, isFetching, enabled e refetch

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,4 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
+import { sleep } from "./sleep";
+import { useState } from "react";
 
 interface IUser {
   id: string;
@@ -7,9 +9,17 @@ interface IUser {
 }
 
 export function Users() {
-  const { data } = useQuery({
+  /*
+    A propriedade enabled é "reativa" então podemos passar um state para ela
+  */
+  // const [enabled, setEnabled] = useState(false);
+
+  const { data, isLoading, refetch, isPending, isFetching } = useQuery({
+    // enabled: false, estou dizendo para o react-query que essa queryFn mão deve ser executada, para que não execute assim que o componente seja montado
+    enabled: false,
     queryKey: ["users"],
     queryFn: async (): Promise<IUser[]> => {
+      await sleep();
       const response = await fetch("http://localhost:3000/users");
 
       return response.json();
@@ -17,7 +27,17 @@ export function Users() {
   });
 
   return (
-    <div>
+    <div className="p-4">
+      <button
+        className="bg-white text-black px-4 py-2 rounded-lg block"
+        type="button"
+        onClick={() => refetch()}
+        // onClick={() => setEnabled(true)}
+      >
+        listar usuarios
+      </button>
+      {isLoading && <h1>Carregando...</h1>}
+      {!isLoading && isFetching && <small>Fetching...</small>}
       {data?.map((user) => (
         <div key={user.id}>
           <strong className="block">{user.name}</strong>
