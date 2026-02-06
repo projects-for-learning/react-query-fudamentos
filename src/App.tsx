191a87c14b02219de90f6d71c0dc249f7445b785
HenriquePereiraa <henrique802011@gmail.com>
2026-02-05 21:11:32 -0300

    feat: debugando a aplicacao com reqct query devTools

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { Users } from "./Users";
 
 const queryClient = new QueryClient();
@@ -7,6 +9,7 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Users />
+      <ReactQueryDevtools buttonPosition="bottom-right" />
     </QueryClientProvider>
   );
 }
