c16977938757fa62968f020dbce7aeb73077a4dd
HenriquePereiraa <henrique802011@gmail.com>
2026-02-05 21:32:23 -0300

    feat:controlando o tempo em que os dados ficam cache

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,28 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { Users } from "./Users";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Posts } from "./Posts";
 
 const queryClient = new QueryClient();
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Users />
-      <ReactQueryDevtools buttonPosition="bottom-right" />
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/">Usuarios</Link>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/posts" element={<Posts />} />
+        </Routes>
+        <ReactQueryDevtools buttonPosition="bottom-right" />
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
---
+++ src/Posts.tsx
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { sleep } from "./sleep";
+
+interface IUser {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export function Posts() {
+  const { data } = useQuery({
+    // enabled: false, estou dizendo para o react-query que essa queryFn mão deve ser executada, para que não execute assim que o componente seja montado
+    enabled: true,
+    staleTime: 5000,
+    queryKey: ["users"],
+    queryFn: async (): Promise<IUser[]> => {
+      await sleep();
+      const response = await fetch("http://localhost:3000/users");
+
+      return response.json();
+    },
+  });
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
