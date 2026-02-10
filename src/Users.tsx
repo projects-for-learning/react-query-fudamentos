a565fe00f5892467c490c046e83affda6ded8eb1
HenriquePereiraa <henrique802011@gmail.com>
2026-02-09 21:24:06 -0300

    feat: criando nossa primeira mutation

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,4 +1,5 @@
 import { useUsers } from "./hooks/useUsers";
+import { useMutation } from "@tanstack/react-query";
 
 export function Users() {
   /*
@@ -8,8 +9,56 @@ export function Users() {
 
   const { users, isLoading, refetch, isFetching, error } = useUsers();
 
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      console.log("mutation executou");
+    },
+  });
+
+  function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    // pegando os valores do input de form uncontrolled
+    /*
+      Tenha o seu mesmo tipo  -> as typeof event.currentTarget.elements
+      E adicionei os inputs -> & {
+      name: HTMLInputElement;
+      email: HTMLInputElement;
+    }
+    */
+    const element = event.currentTarget
+      .elements as typeof event.currentTarget.elements & {
+      name: HTMLInputElement;
+      email: HTMLInputElement;
+    };
+
+    console.log("Nome:", element.name.value);
+    console.log("E-mail:", element.email.value);
+
+    mutate();
+  }
+
   return (
     <div className="p-4">
+      <div className="mb-10">
+        <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+          <input
+            className="outline-none rounded-md text-zinc-900 pl-2"
+            placeholder="Nome"
+            name="name"
+          />
+          <input
+            className="outline-none rounded-md text-zinc-900 pl-2"
+            placeholder="E-mail"
+            name="email"
+          />
+
+          <button className="bg-blue-400 py-2 text-zinc-950 rounded-md">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+
       <button
         className="bg-white text-black px-4 py-2 rounded-lg block"
         type="button"
